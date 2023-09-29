da29867ead8609089481e4d62ad212cd42a7d44a
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-29 22:02:40 +0300

    views

--- Url-shortner/urlshortner/shortner/views.py
+++ Url-shortner/urlshortner/shortner/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+import uuid
 # Create your views here.
 def index(request):
     return render(request, 'shortner/index.html')
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        url = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
