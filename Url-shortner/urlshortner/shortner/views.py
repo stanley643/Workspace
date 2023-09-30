9e47d0d0720054e0867d8fc799ec5d74e8f5974f
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-30 22:33:26 +0300

    readme

--- Url-shortner/urlshortner/shortner/views.py
+++ Url-shortner/urlshortner/shortner/views.py
@@ -8,8 +8,8 @@ def index(request):
 
 def create(request):
     if request.method == 'POST':
-        url = request.POST['link']
+        link = request.POST['link']
         uid = str(uuid.uuid4())[:5]
-        new_url = Url(lin=url, uuid=uid)
+        new_url = Url(link=link, uuid=uid)
         new_url.save()
         return HttpResponse(uid)
