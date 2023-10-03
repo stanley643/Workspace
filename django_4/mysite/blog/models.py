02cdf06f94902eee31b9d300c14e6a4f08846a12
Stanley643 <nmurigi@kabarak.ac.ke>
2023-10-03 08:39:52 +0300

    user

--- django_4/mysite/blog/models.py
+++ django_4/mysite/blog/models.py
@@ -11,6 +11,9 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
