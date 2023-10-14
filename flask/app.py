fc99b65522ac9aee819afdd27c2f52743bb80f3d
Stanley643 <nmurigi@kabarak.ac.ke>
2023-10-14 22:10:18 +0300

    flask

--- flask/app.py
+++ flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from views import views
 app = Flask(__name__)
 
-app.register_blueprint(views, url_prefix="/views")
+app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
     app.run(debug=True)
