efb3ebde304959001a2ec93b190c1dac248953b0
Stanley643 <nmurigi@kabarak.ac.ke>
2023-10-15 22:01:31 +0300

    views

--- flask/views.py
+++ flask/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
@@ -21,3 +21,7 @@ def get_json():
 def get_data():
     data = request.json
     return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.get_json"))
