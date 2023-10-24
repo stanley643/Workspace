ff95106918bcc4430bf77fbf8eb22c704b27635c
Stanley643 <nmurigi@kabarak.ac.ke>
2023-10-24 13:15:54 +0300

    hash table

--- hashtables/hashtable.c
+++ hashtables/hashtable.c
@@ -13,6 +13,7 @@ typedef struct {
 
 }person;
 
+person * hash_table
 unsigned int hash(char *name)
 {
 	int length = strnlen(name, MAX_NAME);
@@ -24,13 +25,34 @@ unsigned int hash(char *name)
 	}
 	return hash_value;
 }
+void init_hash_table()
+{
+	for (int i=0; i<TABLE_SIZE; i++)
+	{
+		hash_table[i] = NULL;
+	}
+}
 
+void print_table()
+{
+	for (int i=0; i< TABLE_SIZE; i++)
+	{
+		if(hash_table[i] ==NULL)
+		{
+			printf("\t%i\t---\n");
+		}
+		else{
+			printf("\t%i\t%s\n", hash_table[i]->name);
+		}
 int main() 
 {
+	init_hash_table();
+	print_table();
+/**
 	printf("Jacob => %u\n", hash("Jacob"));	
 	printf("Stan => %u\n", hash("Stan"));
 	printf("Mike => %u\n", hash("Mike"));
 	printf("Mary => %u\n", hash("Mary"));
-
+*/
 	return 0;
 }
