058a494a535518441559cfec9b4bc949e67aa6e7
Stanley643 <nmurigi@kabarak.ac.ke>
2023-10-25 14:22:01 +0300

    forms

--- django_4/mysite/mysite/settings.py
+++ django_4/mysite/mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'stannjoroge643@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
