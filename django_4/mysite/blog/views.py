10222f35787d05598997916e091baf9e8eadbb98
Stanley643 <nmurigi@kabarak.ac.ke>
2023-10-26 17:39:30 +0300

    forms

--- django_4/mysite/blog/views.py
+++ django_4/mysite/blog/views.py
@@ -4,6 +4,7 @@ from django.http import Http404
 from  .models import *
 from django.core.paginator import Paginator
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 # Create your views here.
 def post_list(request):
     post_list = Post.published.all()
@@ -34,12 +35,24 @@ def post_detail(request, id):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f"{cd['name']} recommends you read " \
+                        f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                        f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'stannjoroge643@gmail.com',
+                      [cd['to']])
+            sent = True
 
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form})
+                                                    'form': form,
+                                                    'sent': sent})
