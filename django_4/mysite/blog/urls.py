58608595b85281a093e410dbade256d39e09dd9c
Stanley643 <nmurigi@kabarak.ac.ke>
2023-10-28 21:01:51 +0300

    forms

--- django_4/mysite/blog/urls.py
+++ django_4/mysite/blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     #path('', views.PostListView.as_view(), name='post_list'),
-    path('<slug:post>/', #intyear>/<int:month>/<int:day>/<slug:post
+    path('<slug:post>/', #int:year>/<int:month>/<int:day>/<slug:post
          views.post_detail, 
          name='post_detail'),
     path('<int:post_id>/share/',
