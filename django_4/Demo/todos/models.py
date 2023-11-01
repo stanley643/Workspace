6c5900898e3f7bc8913e81a246bd267ca6abb775
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-01 22:05:29 +0300

    demo

--- django_4/Demo/todos/models.py
+++ django_4/Demo/todos/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class todo(models.Model):
     title = models.CharField(max_length=250)
-    time = models.DateTimeField
-    completed = models.BooleanField
+    time = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
--- django_4/Demo/todos/views.py
+++ django_4/Demo/todos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from .models import todo
 
 # Create your views here.
 def home(request):
-    return render(request, 'todos/home.html')
+    item = todo.objects.all()
+    return render(request, 'todos/home.html', {"todo":item})
