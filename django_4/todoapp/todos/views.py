eb33e4f56984274d512c69b98f58ffa12f0adf91
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-02 13:13:11 +0300

    django templates

--- django_4/todoapp/todos/views.py
+++ django_4/todoapp/todos/views.py
@@ -5,5 +5,5 @@ def home(request):
     return HttpResponse('Welcome to Todo app')
 
 def todos(request):
-    items = todos.objects.all()
-    return(render, 'todos/index.html', {'todos':items})
+    item = todos.objects.all()
+    return render(request, 'todos/index.html', {'todos':item})
