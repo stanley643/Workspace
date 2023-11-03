93a2188006d5cac862e75624a217d4bde0301af1
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-03 22:28:39 +0300

    new project web

--- django_4/web/mysite/models.py
+++ django_4/web/mysite/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class certificate(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(max_length=250)
+    #image = models.ImageField()
+    issued_on = models.DateTimeField(default=True)
