41c27739ae0c2ccb8ce16ae1902bc7f9abb2734c
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-03 22:10:15 +0300

    rm

--- django_4/Demo/todos/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class todo(models.Model):
-    title = models.CharField(max_length=250)
-    time = models.DateTimeField(auto_now=True)
-    completed = models.BooleanField(default=False)
--- django_4/Demo/todos/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from .models import todo
-
-# Create your views here.
-def home(request):
-    item = todo.objects.all()
-    return render(request, 'todos/home.html', {"todo":item})
