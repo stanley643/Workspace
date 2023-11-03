061244962cd44b11f164fcab43f2ea622e79d0a5
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-03 22:57:40 +0300

    new project web

--- django_4/web/mysite/urls.py
+++ django_4/web/mysite/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('cert/', views.cert, name='cert'),
 ]
--- django_4/web/mysite/views.py
+++ django_4/web/mysite/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import certificates
 # Create your views here.
 
 def home(request):
     return HttpResponse('Hello Stan')
+
+def cert(request):
+    items = certificates.objects.all()
+
+    return render(request, 'mysite/index.html', {'certificate':items})
