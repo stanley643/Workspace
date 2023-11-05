01647ddad8779fa246fe12e6acd7ed012645a250
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-05 22:15:30 +0300

    index

--- django_4/web/mysite/models.py
+++ django_4/web/mysite/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class certificates(models.Model):
     title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     description = models.TextField(max_length=2000)
     #image = models.ImageField()
     issued_on = models.DateTimeField(auto_now=False)
