4441521694f4289a9d4da3218dab9adab32e1a9a
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-05 22:25:06 +0300

    index

--- django_4/web/mysite/admin.py
+++ django_4/web/mysite/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(certificates)
+#admin.site.register(certificates)
+
+@admin.register(certificates)
+class CertAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
