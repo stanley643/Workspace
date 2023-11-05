47e7798583288c2e5beac48e023ce7745678982f
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-05 21:49:34 +0300

    rm

--- django_4/todoapp/todos/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class todos(models.Model):
-    task = models.CharField(max_length=250)
-    time = models.DateTimeField(default=False)
-    completed = models.BooleanField(default=False)
-    comments = models.TextField(max_length=250)
--- django_4/todoapp/todos/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from .models import todos
-# Create your views here.
-def home(request):
-    return HttpResponse('Welcome to Todo app')
-
-def todoapp(request):
-    item = todos.objects.all()
-    return render(request, 'todos/index.html', {'todos':item})
