9263ca909a98bd5f5792f082d187c79b5b6ae33c
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-11 08:51:02 +0300

    macros

--- Alx/macros.c
+++ Alx/macros.c
@@ -4,5 +4,17 @@
 
 int main(int argc, char *argv[])
 {
+	int arr[SIZE];
+	int i;
+
+	for (i = 0; i < SIZE; i++)
+	{
+		arr[i] = i * 2;
+	}
+
+	for (i = 0; i < SIZE; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
 	return 0;
 }
